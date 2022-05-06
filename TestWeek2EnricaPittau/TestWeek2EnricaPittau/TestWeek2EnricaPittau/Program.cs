ef5bd5dc7a5d76765a87e25f5b3b35a057f633c4
EnricaPittau <101341843+EnricaPittau@users.noreply.github.com>
2022-05-06 06:05:41 -0700

    Add files via upload

---
+++ TestWeek2EnricaPittau/TestWeek2EnricaPittau/TestWeek2EnricaPittau/Program.cs
@@ -0,0 +1,53 @@
+﻿// See https://aka.ms/new-console-template for more information
+using TestWeek2EnricaPittau;
+using TestWeek2EnricaPittau.Chain;
+using TestWeek2EnricaPittau.Handler;
+
+Console.WriteLine("Gestione spese!");
+Console.WriteLine("Visualizza le spese inserite.");
+
+List<Spese> listaFile = RepositorySpeseFile.GetAll();
+
+
+
+//IRepository<Spese> repoSpese = new RepositorySpeseFile();
+
+
+//var listaRecuperata = repoSpese.GetAll();
+var listaRecuperata = RepositorySpeseFile.GetAll();
+if (listaRecuperata.Count == 0)
+{
+    Console.WriteLine("Lista vuota");
+}
+else
+{
+    Console.WriteLine("Ecco l'elenco delle spese: ");
+    foreach (var r in listaRecuperata)
+    {
+        Console.WriteLine(r.InfoSpesa());
+    }
+}
+
+
+
+IHandler manager = new Manager();
+IHandler operationalManager = new OperationalManager();
+IHandler ceo = new Ceo();
+
+
+//stabilisco l'ordine in cui verrà gestita la richiesta dall'inizio alla fine
+manager.SetNext(operationalManager).SetNext(ceo);
+
+foreach (var item in listaRecuperata)
+{
+    if (item.Importo > 2500)
+    {
+        Console.WriteLine("Spesa non approvata");
+    }
+    else 
+    {
+        Console.WriteLine("Spesa approvata");
+    }
+
+}
+
---
+++ TestWeek2EnricaPittau/TestWeek2EnricaPittau/TestWeek2EnricaPittau/RepositorySpeseFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestWeek2EnricaPittau
+{
+    public class RepositorySpeseFile
+    {   
+        public static List<Spese> GetAll()
+        {
+            string path = @"C:\Users\enrica.pittau\Desktop\TestWeek2EnricaPittau\TestWeek2EnricaPittau\TestWeek2EnricaPittau\Spese.txt";
+
+            List<Spese> spese = new List<Spese>();
+            using (StreamReader sr = new StreamReader(path))
+            {
+                string contenutoFile = sr.ReadToEnd();
+
+                if (string.IsNullOrEmpty(contenutoFile))
+                {
+                    return spese;
+                }
+                else
+                {
+                    var righeDelFile = contenutoFile.Split("\r\n");
+                    for (int i = 0; i < righeDelFile.Length - 1; i++)
+                    {
+                        var campiDellaRiga = righeDelFile[i].Split(",");
+                        Spese r = new Spese();
+                        r.Data = DateTime.Parse(campiDellaRiga[0]);
+                        r.Categoria = campiDellaRiga[1];
+                        r.Descrizione = campiDellaRiga[2];
+                        r.Importo = double.Parse(campiDellaRiga[3]);
+                        spese.Add(r);
+                    }
+                }
+                return spese;
+            }
+        }
+    }
+}
